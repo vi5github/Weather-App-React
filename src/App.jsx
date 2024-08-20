37b89c2395d6b388a8793060db931d037441bc41
vi5github <varunnissrani@gmail.com>
2024-08-21 01:26:27 +0530

    DEveloped the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Temperature from './components/Temperature'
+import Highlights from './components/Highlights'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [city, setcity] = useState("New Delhi")
+  const [weatherData, setweatherData] = useState(null)
+
+
+  const apiURL = `https://api.weatherapi.com/v1/current.json?key=f6f99450382a4bfa84390735242304&q=${city}&aqi=no`
+
+  useEffect(() => {
+    fetch(apiURL)
+    .then((response) => {
+      if(!response.ok){
+        throw new Error("Error");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      setweatherData(data);
+    })
+    .catch((e) => {
+      console.log(e);
+    })
+  }, [city])
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='bg-[#1F213A] h-screen flex justify-center align-top'>
+        <div className="mt-40 w-1/5 h-1/3">
+           <Temperature
+            setcity = {setcity}
+           />
+
+        </div>
+        <div className="mt-40 w-1/3 h-1/3 p-10 grid grid-cols-2 gap-6">
+            <h2 className='text-slate-200 text-2xl col-span-2'>Today's Highlights</h2>
+            <Highlights/>
+            <Highlights/>
+            <Highlights/>
+            <Highlights/>
+        </div>
+    </div>
   )
 }
 
 export default App
+ 
