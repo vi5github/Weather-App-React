025c68dac8a26f876e3b73df0335b2fdc4872654
vi5github <varunnissrani@gmail.com>
2024-08-21 02:07:09 +0530

    Completed the Project added the functionality using fetch api

--- src/App.jsx
+++ src/App.jsx
@@ -32,18 +32,57 @@ function App() {
 
   return (
     <div className='bg-[#1F213A] h-screen flex justify-center align-top'>
-        <div className="mt-40 w-1/5 h-1/3">
-           <Temperature
-            setcity = {setcity}
-           />
-
-        </div>
+         <div className="mt-40 w-1/5 h-1/3">
+        {weatherData && (
+          <Temperature
+            setcity={setcity}
+            stats={{
+              temp: weatherData.current.temp_c,
+              condition: weatherData.current.condition.text,
+              isDay: weatherData.current.is_day,
+              location: weatherData.location.name,
+              time: weatherData.location.localtime
+            }}
+          />
+        )}
+      </div>
         <div className="mt-40 w-1/3 h-1/3 p-10 grid grid-cols-2 gap-6">
             <h2 className='text-slate-200 text-2xl col-span-2'>Today's Highlights</h2>
-            <Highlights/>
-            <Highlights/>
-            <Highlights/>
-            <Highlights/>
+            
+            {
+              weatherData &&
+              (
+                <>
+                  <Highlights
+                    stats = {{
+                      title: "Wind Status",
+                      value: weatherData.current.wind_mph,
+                      unit:"mph",
+                      direction:weatherData.current.wind_dir
+                    }}
+                  />
+                  <Highlights stats = {{
+                      title: "Humidity",
+                      value: weatherData.current.humidity,
+                      unit:"%",
+                      // direction:weatherData.current.wind_dir
+                    }}/>
+                  <Highlights stats = {{
+                      title: "Visibility",
+                      value: weatherData.current.vis_miles,
+                      unit:"miles",
+                      // direction:weatherData.current.wind_dir
+                    }}/>
+                  <Highlights stats = {{
+                      title: "Air Pressure",
+                      value: weatherData.current.pressure_mb,
+                      unit:"mb",
+                      // direction:weatherData.current.wind_dir
+                    }}/>
+                </>
+              )
+            }
+
         </div>
     </div>
   )
--- src/components/Temperature.jsx
+++ src/components/Temperature.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Temperature = ({setcity}) => {
+const Temperature = ({setcity, stats}) => {
 
     const handleCityChange = (e) =>{
         // console.log(e.target.value);
@@ -10,7 +10,7 @@ const Temperature = ({setcity}) => {
   return (
     <>
         <div className='flex justify-between'>
-            <input type="text" className='bg-slate-600 border-slate-500 text-slate-200 placeholder-color-400 text-md w-60 p-2 focus:outline-none focus:border-slate-400'
+             <input type="text" className='bg-slate-600 border-slate-500 text-slate-200 placeholder-color-400 text-md w-60 p-2 focus:outline-none focus:border-slate-400'
             placeholder='Enter Your City Name'
             onChange={handleCityChange}
             defaultValue="New Delhi"
@@ -25,29 +25,35 @@ const Temperature = ({setcity}) => {
         </div>
 
         <div className='flex justify-center mt-8'>
-            {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-14 h-14 text-yellow-300 text-transfrom scale-100 hover:scale-110 transition-transform duration-300 ease-in-out">
+        {stats.isDay !== 0 ? (
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-14 h-14 text-yellow-300 text-transfrom scale-100 hover:scale-110 transition-transform duration-300 ease-in-out">
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
-            </svg> */}
-
+            </svg> 
+        ):
+        (
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12 text-slate-200 text-transfrom scale-100 hover:scale-110 transition-transform duration-300 ease-in-out">
             <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
             </svg>
+        )}
+            
+
+            
 
         </div>
 
                 
         <div className="flex justify-center items-center text-slate-200 mt-8 text-transfrom scale-100 hover:scale-110 transition-transform duration-300 ease-in-out">
-                <p className='font-semibold text-[55px]'>23
+                <p className='font-semibold text-[55px]'>{stats.temp}
                 </p>
                 <span className='text-[33px]'>°C</span>
         </div>
 
         <div className="flex justify-center items-center text-slate-300 mt-8 text-[25px] text-transfrom scale-100 hover:scale-110 transition-transform duration-300 ease-in-out">
-            Mist
+            {stats.condition}
         </div>
 
         <div className="flex justify-center text-slate-400 mt-5 text-[15px]">
-            Today 2023-11-19  0:19 | Mumbai
+            Today {stats.time} | {stats.location}
         </div>
 
 
